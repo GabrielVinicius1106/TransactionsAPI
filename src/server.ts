fbcc5557ae15342b838fab870f10269fb79a9999
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2026-03-02 18:35:11 -0300

    adding host 0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { app } from "./app";
 import { env } from "./env/index";
 
+const host = '0.0.0.0'
+
 app.listen({
-    port: env.PORT || 3000
+    port: env.PORT || 3000,
+    host: host
 }).then(() => console.log(`Server running on PORT: ${env.PORT || 3000}`) )
